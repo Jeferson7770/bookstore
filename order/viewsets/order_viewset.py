ddf8e7b31d6bf0d29088bd5384ccafed1e6ee178
jeferson7770 <jefersonmoreira770@gmail.com>
2026-08-29 22:12:28 -0300

    Liberando acesso de sessao para ver pedidos no navegador

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,4 +1,4 @@
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -9,5 +9,6 @@ from order.serializers import OrderSerializer
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by("id")
-    authentication_classes = [TokenAuthentication]
+
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated]
